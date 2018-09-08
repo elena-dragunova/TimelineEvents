3fc1aee0a5e163678b0140ee8c1f846ba691ebd4
elena-dragunova <33746898+elena-dragunova@users.noreply.github.com>
2018-09-08 17:20:10 +0500

    Bag fixing

--- src/app/common/mock-events.ts
+++ src/app/common/mock-events.ts
@@ -2,4 +2,12 @@ import { Event } from './event';
 import { NEWS } from './mock-news';
 import { TRANSACTIONS } from './mock-transactions';
 
-export let EVENTS: Event[] = NEWS.concat(TRANSACTIONS);
+export let EVENTS: Event[] = []; //= NEWS.concat(TRANSACTIONS);
+
+for (let i = 0; i < NEWS.length; i++) {
+    EVENTS.push(NEWS[i]);
+}
+
+for (let i = 0; i < TRANSACTIONS.length; i++) {
+    EVENTS.push(TRANSACTIONS[i]);
+}
